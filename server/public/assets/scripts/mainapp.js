c014c6c25118d2c3f36e7e4bfc579a44e03043cf
colinmiller <colinchmiller@gmail.com>
2016-10-10 14:48:24 -0500

    click to reveal content and hide all others if they are displayed

--- server/public/assets/scripts/mainapp.js
+++ server/public/assets/scripts/mainapp.js
@@ -1,50 +1,76 @@
 var mainApp = angular.module('mainApp', []);
 
-mainApp.controller('TechSkillsController', ['$scope', function($scope){
+mainApp.controller('RevealController', ['$scope', function($scope){
   $scope.showTech = false;
-  $scope.techSkills = ['HTML', 'CSS', 'javascript', 'jQuery', 'angular', 'PHP', 'SQL', 'mongoDB',
- 'node.js', 'express', 'Grunt', 'bootstrap', 'XAAMP', 'wordpress'];
-  $scope.toggleShow = function(){
-    if ($scope.showTech == false){
-      $scope.showTech = true;
-    } else if ($scope.showTech == true){
+  $scope.showExperience = false;
+  $scope.showEducation = false;
+  $scope.showProjects = false;
+  $scope.toggleShow = function(target){
+    console.log(target);
+    switch(target){
+      case 'techSkills':
+      $scope.showExperience = false;
+      $scope.showEducation = false;
+      $scope.showProjects = false;
+      if ($scope.showTech == false){
+        $scope.showTech = true;
+      } else if ($scope.showTech == true){
+        $scope.showTech = false;
+      } break;
+
+      case 'pastJobs':
       $scope.showTech = false;
+      $scope.showEducation = false;
+      $scope.showProjects = false;
+      if ($scope.showExperience == false){
+        $scope.showExperience = true;
+      } else if ($scope.showExperience == true){
+        $scope.showExperience = false;
+      } break;
+
+      case 'pastSchools':
+      $scope.showTech = false;
+      $scope.showExperience = false;
+      $scope.showProjects = false;
+      if ($scope.showEducation == false){
+        $scope.showEducation = true;
+      } else if ($scope.showEducation == true){
+        $scope.showEducation = false;
+      } break;
+
+      case 'projects':
+      $scope.showTech = false;
+      $scope.showExperience = false;
+      $scope.showEducation = false;
+      if ($scope.showProjects == false){
+        $scope.showProjects = true;
+      } else if ($scope.showProjects == true){
+        $scope.showProjects = false;
+      } break;
+
+      default:
+      $scope.showTech = false;
+      $scope.showExperience = false;
+      $scope.showEducation = false;
+      $scope.showProjects = false;
+
     }
   }
+}]);
+
+mainApp.controller('TechSkillsController', ['$scope', function($scope){
+  $scope.techSkills = ['HTML', 'CSS', 'javascript', 'jQuery', 'angular', 'PHP', 'SQL', 'mongoDB',
+ 'node.js', 'express', 'Grunt', 'bootstrap', 'XAAMP', 'wordpress'];
 }]);
 
 mainApp.controller('ExperienceController', ['$scope', function($scope){
-  $scope.showExperience = false;
   $scope.pastJobs = ['Eleven Design Co', 'Braun Intertec', 'Geek Squad', 'StoryStick Marketing'];
-  $scope.toggleShow = function(){
-    if ($scope.showExperience == false){
-      $scope.showExperience = true;
-    } else if ($scope.showExperience == true){
-      $scope.showExperience = false;
-    }
-  }
 }]);
 
 mainApp.controller('EducationController', ['$scope', function($scope){
-  $scope.showEducation = false;
   $scope.pastSchools = ['Prime Digital Academy', 'DePaul University'];
-  $scope.toggleShow = function(){
-    if ($scope.showEducation == false){
-      $scope.showEducation = true;
-    } else if ($scope.showEducation == true){
-      $scope.showEducation = false;
-    }
-  }
 }]);
 
 mainApp.controller('ProjectController', ['$scope', function($scope){
-  $scope.showProjects = false;
   $scope.projects = ['Conscious Shopper', 'PHP CMS Blog', 'Old Version Resume'];
-  $scope.toggleShow = function(){
-    if ($scope.showProjects == false){
-      $scope.showProjects = true;
-    } else if ($scope.showProjects == true){
-      $scope.showProjects = false;
-    }
-  }
 }])
